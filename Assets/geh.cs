e2944c0da6bd752612a0882f316e800f603e9810
MediaTransformationJudithLukas <41335072+MediaTransformationJudithLukas@users.noreply.github.com>
2018-07-25 19:48:31 +0200

    Add files via upload

---
+++ Assets/geh.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class geh : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed;
+
+    private int current;
+
+    void Update()
+    {
+		// wenn die Position des Objekts nicht gleich der Position des Targets ist
+        if (transform.position != target[current].position)
+        {
+			//Objekt bewegt sich in einer festgelegten Zeit auf der Z-Achse von Startposition zu Targetposition
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+
+        }
+        else current = (current + 1) % target.Length;
+    }
+}
